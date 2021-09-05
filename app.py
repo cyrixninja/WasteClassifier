e883a2de75bf7e621890abbb6b1d20e2667797bd
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2021-09-05 22:58:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,27 @@ from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from PIL import Image, ImageOps
 import numpy as np
+import base64
+
+st.title(' ')
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+       data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+        <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+        ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('image.png')
 
 
 def classifier(img, file):
