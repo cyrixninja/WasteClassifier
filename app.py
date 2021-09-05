0dfdfa6e5f7afbeb1fc66be9817ab28c30f73c2f
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2021-09-05 22:59:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def set_background(png_file):
         ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-set_background('image.png')
+set_background('sun-tornado.png')
 
 
 def classifier(img, file):
